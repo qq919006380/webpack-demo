8ba466e57b8a70a2b5c07d56ba45a620e3a41d5b
weibo <919006380@qq.com>
2021-04-16 21:59:04 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,20 +1,12 @@
 import _ from 'lodash';
-import printMe from './print.js';
-
-
 
 function component() {
     const element = document.createElement('div');
-    const btn = document.createElement('button');
-
-    // lodash 在当前 script 中使用 import 引入
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
   
-    element.appendChild(btn);
+    // lodash（目前通过一个 script 引入）对于执行这一行是必需的
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  
     return element;
-}
-
-document.body.appendChild(component());
+  }
+  
+  document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,9 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-
-    entry: {
-        index: './src/index.js',
-        another: './src/another-module.js',
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Development',
-
-        }),
-    ],
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-        publicPath: '/',
-
-    },
-
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
 };
